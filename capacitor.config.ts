b8597be11c4174ce649c8adbe73bb980d6e82369
BaiYanchen <2786660867@qq.com>
2024-04-25 23:24:06 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'blive-monitor-send',
+  webDir: 'dist'
+};
+
+export default config;
